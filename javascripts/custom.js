3ebaf06eda8932c21748dd814694e8e52367276a
Simonova <zonnya@mail.ru>
2017-10-14 17:40:31 +0300

    supporters block

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -19,7 +19,48 @@ $(document).ready(function () {
     var min_h_content = $(window).height() - $('.header').height() - $('.footer').height();
     $('.content').css('min-height', min_h_content);
 
-    
+    $('.supporters__slider').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      prevArrow: '<div class="slick-arrow slick-prev"><i class="fa fa-chevron-left" aria-hidden="true"></i></div>',
+      nextArrow: '<div class="slick-arrow slick-next"><i class="fa fa-chevron-right" aria-hidden="true"></i></div>',
+      // appendArrows: $('.simple_slider__nav_arr'),
+      arrows: true,
+      // fade: true,
+      dots: false,
+      responsive: [
+        {
+          breakpoint: 1365,
+          settings: {
+            slidesToShow: 5
+          }
+        },
+        {
+          breakpoint: 1099,
+          settings: {
+            slidesToShow: 4
+          }
+        },
+        {
+          breakpoint: 909,
+          settings: {
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 712,
+          settings: {
+            slidesToShow: 2
+          }
+        },
+        {
+          breakpoint: 479,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
 
     $(document).click(function(event) {
      
