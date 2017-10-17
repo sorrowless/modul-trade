17bc1f8fbfcceef48419e05bf5cf1fb4a3c5501f
Simonova <zonnya@mail.ru>
2017-10-17 05:19:51 +0300

    add_new_block_and_fix_produc_img

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -62,6 +62,37 @@ $(document).ready(function () {
       ]
     });
 
+    var users_amount__chart__percent = 1;
+    var users_amount__chart__size = 180;
+    var users_amount__chart__lineWidth = 20;
+    var users_amount__chart__rotate =  0;
+    var users_amount__chart__radius = (users_amount__chart__size - users_amount__chart__lineWidth) / 2;
+
+
+    $('.users_amount__chart_line').each(function(){
+      var users_amount__chart__percent = $(this).attr('line-width') / 100;
+      var users_amount__chart__size = $(this).width();
+      users_amount__chart__radius = (users_amount__chart__size - users_amount__chart__lineWidth) / 2;
+      
+      var canvas = document.createElement('canvas');
+      canvas.width = canvas.height = users_amount__chart__size;
+      var ctx = canvas.getContext('2d');
+      $(this).append(canvas);
+      ctx.translate(users_amount__chart__size / 2, users_amount__chart__size / 2); 
+      ctx.rotate((-1 / 2) * Math.PI); 
+      var drawCircle = function(color, lineWidth, percent) {
+          percent = Math.min(Math.max(0, users_amount__chart__percent || 1), 1);
+          ctx.beginPath();
+          ctx.arc(0, 0, users_amount__chart__radius, 0, Math.PI * 2 * users_amount__chart__percent, false);
+          ctx.strokeStyle = color;
+          ctx.lineWidth = users_amount__chart__lineWidth;
+          ctx.stroke();
+      };
+
+      drawCircle('#ffffff', users_amount__chart__lineWidth, 100 / 100);
+      drawCircle('#ffaa01', users_amount__chart__lineWidth, users_amount__chart__percent / 100);
+    })
+
     $(document).click(function(event) {
      
       if (!$(event.target).closest(".lang").length){
@@ -71,7 +102,6 @@ $(document).ready(function () {
     $(window).resize(function(){
       min_h_content = $(window).height() - $('.header').height() - $('.footer').height();
       $('.content').css('min-height', min_h_content);
-
     });
 
 
